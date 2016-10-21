f0cb65d8f4096a6d358e77db3269096fdab14602
Deyvis Valdez Huanca <deyvis.vh@gmail.com>
2016-10-20 22:47:46 -0500

    fractal to responses

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/** Rutas del API */
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1', function ($api) {
+    $api->group(['namespace' => 'App\Http\Controllers'], function($api) {
+        $api->resource('users', 'UsersController'); 
+    });
+});
