2df14757e9c0c91ce78a43861cc6c5f18a2e8f1d
Deyvis Valdez Huanca <deyvis.vh@gmail.com>
2016-11-01 18:15:22 -0500

    get user with token

--- app/Providers/OAuthServiceProvider.php
+++ app/Providers/OAuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Auth\UserResolver;
 use Illuminate\Support\ServiceProvider;
 use Dingo\Api\Auth\Provider\OAuth2;
 
@@ -15,14 +16,15 @@ class OAuthServiceProvider extends ServiceProvider
     public function boot()
     {
         app('Dingo\Api\Auth\Auth')->extend('oauth', function ($app) {
-           $provider = new OAuth2($app['oauth2-server.authorizer']->getChecker());
+            $provider = new OAuth2($app['oauth2-server.authorizer']->getChecker());
             
             $provider->setClientResolver(function ($id) {
                 // Logic to return a client by their ID.
             });
 
             $provider->setUserResolver(function ($id) {
-                // Logic to return a user by their ID.
+                $resolver = app(UserResolver::class);
+                return $resolver->resolveById($id);
             });
             
             return $provider;
